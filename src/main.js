022e5c5b04d4954b7f2051a1181b2f9e8ad03ce7
deco <andrefsantos@skiff.com>
2024-07-19 00:16:07 +0100

    router and stores setup

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
-import './style.css'
+import { createPinia } from 'pinia'
+import './main.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import router from './router'
+
+createApp(App)
+  .use(router)
+  .use(createPinia())
+  .mount('#app')
